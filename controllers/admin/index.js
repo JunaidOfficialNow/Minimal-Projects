ee48fcb47e92b0e80b21399ab7a56e3c395bd4f1
Junaid <junaidofficialnow@gmail.com>
2023-04-16 18:14:04 +0530

    product controller expanded

--- controllers/admin/index.js
+++ controllers/admin/index.js
@@ -2,10 +2,12 @@ const auth = require('./authController');
 const userManagement = require('./userController');
 const categoryManagement = require('./categoryController');
 const designCategoryManagement = require('./designController');
+const productManagement = require('./productController');
 
 module.exports = {
   ...auth,
   ...userManagement,
   ...categoryManagement,
   ...designCategoryManagement,
+  ...productManagement,
 };
