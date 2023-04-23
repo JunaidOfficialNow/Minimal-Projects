31aa2cbe76dd626cfb98ce1dbaec75b902bde378
Junaid <junaidofficialnow@gmail.com>
2023-04-23 17:52:23 +0530

    user cart controller expanded

--- controllers/user/index.js
+++ controllers/user/index.js
@@ -1,10 +1,11 @@
 const authManagement = require('./authController');
 const productManagement = require('./productController');
 const addressManagement = require('./addressController');
+const cartManagement = require('./cartController');
 
 module.exports = {
   ...authManagement,
   ...productManagement,
   ...addressManagement,
-
+  ...cartManagement,
 };
