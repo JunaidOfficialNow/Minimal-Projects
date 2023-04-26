0952c2c8f5093638494cda664a14316c37d66ef9
Junaid <junaidofficialnow@gmail.com>
2023-04-26 10:21:31 +0530

    order controller expanded

--- controllers/user/orderController.js
+++ controllers/user/orderController.js
@@ -1,4 +1,5 @@
 const Order = require('../../models/orderModel');
+const Product = require('../../models/productModel');
 
 
 exports.getOrderPage = async (req, res)=> {
@@ -12,4 +13,37 @@ exports.getOrderPage = async (req, res)=> {
     page: 'order',
     orders,
   });
+};
+
+exports.getOrderDetails = async (req, res, next)=> {
+  const order = await Order.findById(req.body.id).populate({
+    path: 'products.product',
+    model: Product,
+  }).catch((err)=> next(err));
+  if (order) {
+    return res.json({success: true, order});
+  }
+  return res.json({success: false});
+};
+
+exports.changeStatusOrder = async (req, res, next)=> {
+  try {
+    const {id, status, products} = req.body;
+    if (status === 'Cancelled') {
+      products.forEach(async (product)=>{
+        // eslint-disable-next-line max-len
+        const filter = {_id: product.product._id};
+        const update = {$inc: {
+          'stock': Number(product.quantity),
+          'sizes.$[elem].stock': Number(product.quantity),
+        }};
+        const options = {arrayFilters: [{'elem.size': product.size}]};
+        await Product.updateOne( filter, update, options);
+      });
+    }
+    await Order.findByIdAndUpdate(id, {status: status});
+    res.json({success: true});
+  } catch (error) {
+    next(error);
+  }
 };
