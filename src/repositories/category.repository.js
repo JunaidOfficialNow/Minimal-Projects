37a42306e9f5f1af6f6a8c516c45f9ac80104cae
Junaid <junaidofficialnow@gmail.com>
2023-05-01 18:29:45 +0530

    refactor: changed repositories to singleton classes

--- src/repositories/category.repository.js
+++ src/repositories/category.repository.js
@@ -1,23 +1,28 @@
+/* eslint-disable require-jsdoc */
 const Category = require('../models/category.model');
 
-exports.checkCategoryExists = (name)=>{
-  return new Promise((resolve, reject)=>{
-    Category.findOne({name: name}).then((doc)=>{
-      if (doc) {
-        // eslint-disable-next-line prefer-promise-reject-errors
-        reject(new Error('Category already exists, should be unique'));
-      } else resolve();
+class CategoryRepository {
+  checkCategoryExists(name) {
+    return new Promise((resolve, reject)=>{
+      Category.findOne({name: name}).then((doc)=>{
+        if (doc) {
+          // eslint-disable-next-line prefer-promise-reject-errors
+          reject(new Error('Category already exists, should be unique'));
+        } else resolve();
+      });
     });
-  });
-};
+  }
 
-exports.checkCategoryExistsById = (id) => {
-  return new Promise((resolve, reject)=>{
-    Category.findById(id).then((doc)=>{
-      if (doc) {
-        resolve(doc.name);
-      } else reject(new Error('Category may have been deleted'));
+  checkCategoryExistsById(id) {
+    return new Promise((resolve, reject)=>{
+      Category.findById(id).then((doc)=>{
+        if (doc) {
+          resolve(doc.name);
+        } else reject(new Error('Category may have been deleted'));
+      });
     });
-  });
-};
+  }
+}
 
+
+module.exports = new CategoryRepository();
