913fce61316d4c50c60e66f62566b00f5fb16673
Junaid <junaidofficialnow@gmail.com>
2023-05-08 16:11:51 +0530

    update: moved product  services to rsl

--- src/controllers/admin/product.controller.js
+++ src/controllers/admin/product.controller.js
@@ -1,124 +1,77 @@
-const Product = require('../../models/product.model');
-const Design = require('../../models/design.model');
 
-exports.getProductsPage = (req, res, next) => {
-  Product.find().then((products)=> {
-    res.render('admins/admin-products', {admin: req.session.admin,
-      products: products});
-  }).catch((err)=> next(err));
-};
+const productServices = require('../../services/product.services');
+const designServices = require('../../services/design.services');
+
+const catchAsync = require('../../utils/error-handlers/catchAsync.handler');
+
+exports.getProductsPage = catchAsync(async (req, res, next) => {
+  const products = await productServices.getAllProducts();
+  res.render('admins/admin-products', {
+    admin: req.session.admin,
+    products,
+  });
+});
 
 exports.addProductName = (req, res) => {
   const {name, designCode} = req.body;
-  req.session.addProduct = {
-    name: name,
-    designCode: designCode,
-  };
+  req.session.addProduct = {name, designCode};
   res.json({success: true});
 };
 
-exports.getAddProductPage = (req, res, next) => {
+exports.getAddProductPage = catchAsync(async (req, res, next) => {
   const {name, designCode} = req.session.addProduct;
-  Design.findOne({designCode}).then((doc)=>{
-    res.render('admins/admin-add-product', {name: name, doc});
-  }).catch((err)=> next(err));
-};
+  const doc = await designServices.getDesignByCode(designCode);
+  res.render('admins/admin-add-product', {name, doc});
+});
 
-exports.addProductAll = async (req, res, next)=> {
-  // need to check the req.body here and use destructuring properly
-  try {
-    const {name, designCode, gender, sizes, exactColor, broadColor, price,
-      stock, category} = req.body;
-    const newSizes = sizes.map((size)=> {
-      return {size: size,
-        stock: req.body[`stockOf${size}`],
-      };
-    });
-    const imageArray = Object.values(req.files).map((file) => file.filename);
-    const details = {
-      name: name,
-      designCode: designCode,
-      gender: gender,
-      sizes: newSizes,
-      exactColor: exactColor,
-      broadColor: broadColor,
-      stock: stock,
-      category: category,
-      price: price,
-      images: imageArray,
-      lastEditedBy: req.session.admin.firstName,
+exports.addProductAll = catchAsync(async (req, res, next)=> {
+  // need to modify the dto from frontend by creating
+  //  the sizes array from the frontend  itselt;
+  const dto = {name, designCode, gender, sizes, exactColor, broadColor, price,
+    stock, category} = req.body;
+  dto[sizes] = sizes.map((size)=> {
+    return {size: size,
+      stock: req.body[`stockOf${size}`],
     };
-    const product = new Product(details);
-    await product.save();
-    res.json({success: true});
-  } catch (error) {
-    next(error);
-  }
-};
+  });
+  dto[images] = Object.values(req.files).map((file) => file.filename);
+  dto[lastEditedBy] = req.session.admin.firstName;
+  await productServices.createNewProduct(dto);
+  res.json({success: true});
+});
 
-exports.changeProductCODStatus = async (req, res, next)=> {
-  try {
-    const product = await Product.findById(req.body.id);
-    if (product) {
-      product.isCodAvailable = !product.isCodAvailable;
-      const newProduct = await product.save();
-      res.json({success: true, status: newProduct.isCodAvailable});
-    } else {
-      throw new Error('Couldn\'t find product');
-    }
-  } catch (error) {
-    next(error);
-  }
-};
+exports.changeProductCODStatus = catchAsync(async (req, res, next)=> {
+  const status = await productServices.updateCODStatus(req.body.id);
+  res.json({success: true, status});
+});
 
-exports.changeProductActiveStatus = async (req, res, next)=> {
-  try {
-    const product = await Product.findById(req.body.id);
-    product.isActive = !product.isActive;
-    const newProduct = await product.save();
-    res.json({success: true, status: newProduct.isActive});
-  } catch (error) {
-    next(error);
-  };
-};
+exports.changeProductActiveStatus = catchAsync(async (req, res, next)=> {
+  const status = await productServices.updateActiveStatus(req.body.id);
+  res.json({success: true, status});
+});
 
-exports.getEditProductPage = (req, res, next)=> {
-  Product.findById(req.params.id).then((product)=> {
-    res.render('admins/edit-product', {product});
-  }).catch((error)=> next(error));
-};
+exports.getEditProductPage = catchAsync(async (req, res, next)=> {
+  const product = await productServices.getProductById(req.params.id);
+  res.render('admins/edit-product', {product});
+});
 
-exports.updateProduct = async (req, res, next)=> {
-  try {
-    // destructuring and repository layer need  to be checked
-    const {name, id, designCode, gender, sizes, exactColor, broadColor, price,
-      stock, category} = req.body;
-    const newSizes = sizes.map((size)=> {
-      return {size: size,
-        stock: req.body[`stockOf${size}`],
-      };
-    });
-    const imageArray = Object.values(req.files).map((file) => file.filename);
-    const details = {
-      name: name,
-      designCode: designCode,
-      gender: gender,
-      sizes: newSizes,
-      exactColor: exactColor,
-      broadColor: broadColor,
-      stock: stock,
-      category: category,
-      price: price,
-      lastEditedBy: req.session.admin.firstName,
+exports.updateProduct = catchAsync(async (req, res, next)=> {
+  // destructuring and repository layer need  to be checked
+  const dto = {name,
+    designCode, gender, sizes, exactColor, broadColor, price,
+    stock, category} = req.body;
+  dto[sizes] = sizes.map((size)=> {
+    return {size: size,
+      stock: req.body[`stockOf${size}`],
     };
-    const product = await Product.findByIdAndUpdate(id, details, {new: true});
-    imageArray.forEach((image)=> {
-      product.images.push(image);
-    });
-    await product.save();
-    res.json({success: true});
-  } catch (error) {
-    next(error);
-  }
-};
+  });
+  const images = Object.values(req.files).map((file) => file.filename);
+  dto[lastEditedBy] = req.session.admin.firstName;
+  await productServices.updateProduct(
+      req.body.id,
+      dto,
+      images,
+  );
+  res.json({success: true});
+});
 
--- src/repositories/product.repository.js
+++ src/repositories/product.repository.js
@@ -5,8 +5,28 @@ class ProductRepository {
     this.#model = model;
   }
 
+  async getAllProducts() {
+    return await this.#model.find();
+  }
+
+  async createNewProduct(data) {
+    return await this.#model.create(data);
+  }
+
   async updateManyProducts(criteria, updates, options) {
     return await this.#model.updateMany(criteria, updates, options);
+  }
+
+  async getProductById(id) {
+    return await this.#model.findById(id);
+  }
+
+  async saveDocument(doc) {
+    return await doc.save();
+  }
+
+  async updateOneById(id, updates, options) {
+    return await this.#model.findByIdAndUpdate(id, updates, options);
   }
 };
 
