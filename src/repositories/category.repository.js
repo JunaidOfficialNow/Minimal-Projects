c331dd14c3833bc41e719849675b76d0cc39177c
Junaid <junaidofficialnow@gmail.com>
2023-05-16 12:52:26 +0530

    feat: privatized model in category repo

--- src/repositories/category.repository.js
+++ src/repositories/category.repository.js
@@ -2,39 +2,40 @@
 
 
 class CategoryRepository {
+  #model;
   constructor(model) {
-    this.model = model;
+    this.#model = model;
   }
   async getCategoryByName(name) {
-    return await this.model.findOne({name: name});
+    return await this.#model.findOne({name: name});
   }
 
   async getCategoryById(id) {
-    return await this.model.findById(id);
+    return await this.#model.findById(id);
   }
 
   async createCategory(data) {
-    return await this.model.create(data);
+    return await this.#model.create(data);
   }
 
   async getAllCategories() {
-    return await this.model.find({});
+    return await this.#model.find({});
   }
 
   async getAllCategoryNames() {
-    return await this.model.find({}).select('name -_id');
+    return await this.#model.find({}).select('name -_id');
   }
 
   async getCategoryName(id) {
-    return await this.model.findById(id).select('name -_id');
+    return await this.#model.findById(id).select('name -_id');
   }
 
   async deleteCategoryById(id) {
-    return await this.model.findByIdAndRemove(id);
+    return await this.#model.findByIdAndRemove(id);
   }
 
   async updateCategoryById(id, updates, options) {
-    return await this.model.findByIdAndUpdate(id, updates, options);
+    return await this.#model.findByIdAndUpdate(id, updates, options);
   }
 }
 
